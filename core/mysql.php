9437693e9bee145d3556a643823edc502f45a282
bruno <freitas.bruno@aluno.ifsp.edu.br>
2022-08-03 11:27:05 -0300

    blog

--- core/mysql.php
+++ core/mysql.php
@@ -5,7 +5,11 @@ function insere(string $entidade, array $dados) : bool
     $retorno = false;
 
     foreach ($dados as $campo => $dados) {
-        
+        $coringa[$campo] = '?';
+        $tipo[] = gettype($dado) [0];
+        $$campo = $dado;
     }
+
+    $instr
 }
 ?>
